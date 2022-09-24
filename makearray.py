ceb8d19ac289abbcebad84e30a281cf05f7c1965
Josh Beaker <trisimix@protonmail.com>
2022-09-24 14:55:45 -0700

    this goblin code makes a static array for jumps

---
+++ makearray.py
@@ -0,0 +1,18 @@
+
+jump_frames = 120
+jump_height = 50
+jumps = [None ] * jump_frames
+counter = 0
+gravity = 9.8
+jumpforce = 69973
+jumps[0] = jumpforce
+for i in range(0, jump_frames):
+    if i != 0:
+        jumps[i] = jumps[i-1] - i*9.8
+normalizer = (max(jumps)/jump_height)
+norm = [float(i)/normalizer for i in jumps]
+norm = list(map(int, norm))
+#norm = norm.reverse()
+#print(jumps)
+norm.reverse()
+print(norm)
